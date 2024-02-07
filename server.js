6ae11fa01a36dd6264df61abdfcce6546544be48
lisavanmansom <lisavanmansom@gmail.com>
2024-02-07 14:36:22 +0100

    styling + inhoud html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import fetchJson from './helpers/fetch-json.js'
 
 // Haal data op uit de FDND API, ga pas verder als de data gedownload is
 const { data } = await fetchJson('https://fdnd.directus.app/items/person/35')
-// console.log(data.data.name) // console.log(data); // uncomment om de opgehaalde data te checken
+// console.log(data.data); //console.log(data); // uncomment om de opgehaalde data te checken
 
 // Maak een nieuwe express app aan
 const app = express()
